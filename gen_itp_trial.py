110a4569289ae9e5b5f23d00f55be9245645e8ae
Magnus Dyrmose Ryseth <magnusdr@pc4909.pc.met.no>
2021-06-17 11:44:54 +0200

    More generalised

--- gen_itp_trial.py
+++ gen_itp_trial.py
@@ -20,6 +20,7 @@ data = pd.read_table(file, skipfooter = 1, header = None, sep='\s+',
 #print(data)
 a0 = np.array([data[0][0],data[1][0],data[2][0],data[3][0]]) #list of ITP number and profile number
 a1 = np.array([data[6][0],data[7][0],data[8][0], 0]) #list of general metadata info - hopefully general
+#can't remember why I added 0 at the end ^
 #a bit of clean-up required, i.e. remove confusing chars
 a00l = list(a0[0])
 a00l[0] = ''
@@ -27,17 +28,20 @@ a0[0] = ''.join(a00l)
 a0m1 = list(a0[-1])
 a0m1[-1] = ''
 a0[-1] = ''.join(a0m1)
-#print(a0,a1) #cleaned, so far a purely cosmetic change - can be used in documentation l8r
+print(a0,a1) #cleaned, so far a purely cosmetic change - can be used in documentation l8r
 
 data = data.drop([0]) 
-data = data.reset_index(drop = True)
 #Have to extract metadata before dropping Na - or else we delete columns which may contain data
-meta = data.iloc[1].drop(data.columns[[0,1]]).dropna(axis = 0).to_numpy()
-print(meta)
-print(data)
-#data.drop([1])
-'''
-data = data.dropna(axis = 1)
+meta = data.iloc[0].dropna(axis = 0).to_numpy() # obtain general metadata
+var = data.iloc[1].drop(data.columns[[0,1]]).dropna(axis = 0).to_numpy() #obtain variable names
+print(meta, var)
+#all necessary info from top rows acquired, can drop these now
+#print(data)
+data = data.drop([1,2]) #drop top two rows, reindexing happens next
+data = data.reset_index(drop = True)
+#print(data)
+
+data = data.dropna(axis = 1) #can finally drop the NaNs safely
 #print(data)
 '''
 b = np.zeros(len(data.columns)-1,'U20')
@@ -50,13 +54,11 @@ for i in range(len(b)-1):
 data = data.astype(float)
 data[0] = data[0].astype(int)
 #back to the file format we have in pdtrial_itp_iter.py, potentially with more measurements per profile
-#print(str(data[0][1]))
-data = data.reset_index(drop = True)
-#print(data[1])
+#data = data.reset_index(drop = True) #done above
 #print(data)
 data['times'] = pd.to_datetime(data[1], unit = 'D', origin = str(data[0][0]))
 data = data.drop(data.columns[[0,1]], axis = 1)
-#b[-1] = 'times'
-#data.columns = b
 #print(data)
-'''
+#b[-1] = 'times'
+#data.columns = #how to combine var with 'times'?
+print(data)
